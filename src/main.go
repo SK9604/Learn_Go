24e3361beb308265a8d611fd08dbac5ef0a705ef
SK9604 <tjsrud428@gmail.com>
2021-01-09 11:53:51 +0900

    naked function, defer

--- src/main.go
+++ src/main.go
@@ -9,14 +9,22 @@ func multiply(a, b int) int { //(a int, b int)와 같다
 	return a * b
 }
 
+/*
 func lenAndUpper(name string) (int, string) { //go는 한번에 2개 이상의 타입을 return 할 수 있다.
 	return len(name), strings.ToUpper(name) //go의 package는 아주 방대하다
 }
+*/
 
 func repeatMe(words ...string) { //같은 타입의 인자를 여러 개 받을 때 .을 이용하여 한 변수로 모두 받을 수 있다.
 	fmt.Println(words)
 }
 
+func lenAndUpper(name string) (length int, uppercase string) { //naked return (미리 리턴할 값을 function을 정의 할 때 설정해준다.)
+	defer fmt.Println("I'm done") //func을 실행시킨 후 끝나면 defer를 수행한다.
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
 func main() {
 	name := "sk" // var name string = "sk"와 같다, func안에서만 작동한다.
 	name = "sk kim"
