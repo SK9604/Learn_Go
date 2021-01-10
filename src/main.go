00e801e4ad7628460ea5153783eac757b8af592b
SK9604 <tjsrud428@gmail.com>
2021-01-10 17:11:23 +0900

    range

--- src/main.go
+++ src/main.go
@@ -2,37 +2,64 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
+/* 기본적인 function의 형식
 func multiply(a, b int) int { //(a int, b int)와 같다
 	return a * b
 }
+*/
 
-/*
+/* go function의 특징 1
 func lenAndUpper(name string) (int, string) { //go는 한번에 2개 이상의 타입을 return 할 수 있다.
 	return len(name), strings.ToUpper(name) //go의 package는 아주 방대하다
 }
 */
 
-func repeatMe(words ...string) { //같은 타입의 인자를 여러 개 받을 때 .을 이용하여 한 변수로 모두 받을 수 있다.
-	fmt.Println(words)
-}
-
+/* go function의 특징 2
 func lenAndUpper(name string) (length int, uppercase string) { //naked return (미리 리턴할 값을 function을 정의 할 때 설정해준다.)
 	defer fmt.Println("I'm done") //func을 실행시킨 후 끝나면 defer를 수행한다.
 	length = len(name)
 	uppercase = strings.ToUpper(name)
 	return
 }
+*/
+
+/* go argument 특징
+func repeatMe(words ...string) { //같은 타입의 인자를 여러 개 받을 때 .을 이용하여 한 변수로 모두 받을 수 있다.
+	fmt.Println(words)
+}
+*/
+
+func superAdd(numbers ...int) int {
+	for index, number := range numbers { //range : loop를 만드는 방법
+		fmt.Println(index, number)
+	}
+	/* index를 무시하고 받은 값들의 합을 보내기
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+	*/
+	/* 일반적인 for문으로 loop 작성하기
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(numbers[i])
+	}
+	*/
+	return 1
+}
+
 func main() {
-	name := "sk" // var name string = "sk"와 같다, func안에서만 작동한다.
-	name = "sk kim"
-	totalLength, upperName := lenAndUpper("sk")
-	length, _ := lenAndUpper("nico") // value값을 무시한다
-	fmt.Println(name)
-	fmt.Println(multiply(2, 2))
-	fmt.Println(totalLength, upperName)
-	fmt.Println(length)
-	repeatMe("seon", "kyeong", "kim")
+	superAdd(1, 2, 3, 4, 5, 6)
+	//name := "sk" // var name string = "sk"와 같다, func안에서만 작동한다.
+	//name = "sk kim"
+	//totalLength, upperName := lenAndUpper("sk")
+	//length, _ := lenAndUpper("nico") // value값을 무시한다
+	//fmt.Println(name)
+	//fmt.Println(multiply(2, 2))
+	//fmt.Println(totalLength, upperName)
+	//fmt.Println(length)
+	//repeatMe("seon", "kyeong", "kim")
+
 }
