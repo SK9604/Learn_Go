442d079abdac6f4ac828c3a6c06fd4fbf64e0895
SK9604 <tjsrud428@gmail.com>
2021-02-20 20:22:50 +0900

    Add Method

--- src/main.go
+++ src/main.go
@@ -86,12 +86,27 @@ type person struct {
 */
 func main() {
 	dictionary := mydict.Dictionary{}
-	definition, err := dictionary.Search("first")
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
 	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found", word, "definition:", hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
+	/*
+		definition, err := dictionary.Search("first")
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(definition)
+		}
+	*/
 	//account := banking.Account{Owner: "sk", Balance: 10000}
 	//fmt.Println(account)
 	/*
