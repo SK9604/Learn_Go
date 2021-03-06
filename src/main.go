f6e88ad78282d02a2051a4f6a2f1e1d085221394
SK9604 <tjsrud428@gmail.com>
2021-03-06 23:22:59 +0900

    정리

--- src/main.go
+++ src/main.go
@@ -108,110 +108,96 @@ func main() {
 	go sexyCount("Kim")
 	time.Sleep(time.Second * 5) //goroutines는 메인 함수가 작동하는 동안에만 동작함
 	/*
-		var results = make(map[string]string)
-		urls := []string{
-			"https://www.airbnb.com/",
-			"https://www.google.com/",
-			"https://www.amazon.com/",
-			"https://www.reddit.com/",
-			"https://www.google.com/",
-			"https://soundcloud.com/",
-			"https://www.facebook.com/",
-			"https://www.instagram.com/",
-			"https://academy.nomadcoders.co/",
-		}
-		for _, url := range urls {
-			result := "OK"
-			err := hitURL(url)
-			if err != nil {
-				result = "FAILED"
-			}
-			results[url] = result
-		}
-		for url, result := range results {
-			fmt.Println(url, result)
-		}
-	*/
-	/*
-		dictionary := mydict.Dictionary{}
-		baseword := "hello"
-		dictionary.Add(baseword, "First")
-		dictionary.Search(baseword)
-		dictionary.Delete(baseword)
-		word, _ := dictionary.Search(baseword)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(word)
-		}
-	*/
-	/*
-		definition := "Greeting"
-		err := dictionary.Add(word, definition)
-		if err != nil {
-			fmt.Println(err)
-		}
-		hello, _ := dictionary.Search(word)
-		fmt.Println("found", word, "definition:", hello)
-		err2 := dictionary.Add(word, definition)
-		if err2 != nil {
-			fmt.Println(err2)
-		}
-	*/
-	/*
-		definition, err := dictionary.Search("first")
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(definition)
-		}
-	*/
-	//account := banking.Account{Owner: "sk", Balance: 10000}
-	//fmt.Println(account)
-	/*
-		account := accounts.NewAccount("sk")
-		account.Deposit(10)
+				var results = make(map[string]string)
+				urls := []string{
+					"https://www.airbnb.com/",
+					"https://www.google.com/",
+					"https://www.amazon.com/",
+					"https://www.reddit.com/",
+					"https://www.google.com/",
+					"https://soundcloud.com/",
+					"https://www.facebook.com/",
+					"https://www.instagram.com/",
+					"https://academy.nomadcoders.co/",
+				}
+				for _, url := range urls {
+					result := "OK"
+					err := hitURL(url)
+					if err != nil {
+						result = "FAILED"
+					}
+					results[url] = result
+				}
+				for url, result := range results {
+					fmt.Println(url, result)
+				}
+				dictionary := mydict.Dictionary{}
+				baseword := "hello"
+				dictionary.Add(baseword, "First")
+				dictionary.Search(baseword)
+				dictionary.Delete(baseword)
+				word, _ := dictionary.Search(baseword)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println(word)
+				}
+				definition := "Greeting"
+				err := dictionary.Add(word, definition)
+				if err != nil {
+					fmt.Println(err)
+				}
+				hello, _ := dictionary.Search(word)
+				fmt.Println("found", word, "definition:", hello)
+				err2 := dictionary.Add(word, definition)
+				if err2 != nil {
+					fmt.Println(err2)
+				}
+				definition, err := dictionary.Search("first")
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println(definition)
+				}
 
-			fmt.Println(account.Balance())
-			err := account.Withdraw(20)
-			if err != nil {
-				fmt.Println(err)
-			}
+			account := banking.Account{Owner: "sk", Balance: 10000}
+			fmt.Println(account)
 
-		fmt.Println(account)
-	*/
-	/*struct
-	favFood := []string{"ricenoodle", "strawberry"}
-	sk := person{"sk", 26, favFood}
-	fmt.Println(sk.name, sk.age, sk.favFood)
-	*/
-	/*
-		sk := map[string]string{"name": "sk", "age": "12"} //key:value 형태
-		fmt.Println(sk)
-		for key, value := range sk {
-			fmt.Println(key, value)
-		}
-	*/
-	/*
-		names1 := [5]string{"sk", "kim", "kyu"} //Array
-		names2 := []string{"sk", "kim", "kyu"}  // Slice = 크기 제한 없는 Array
-		names1[3] = "heon"
-		names1[4] = "jin"
-		//names1[5] = "hee" //array값을 벗어남
-		//names2[3] = "heon" //Slice에 새로운 값을 추가할 경우에는 append함수 사용
-		names2 = append(names2, "jin")
-		fmt.Println(names1)
-		fmt.Println(names2)
-	*/
-	/*a := 2
-	b := a
-	c := &a //& = 주소
-	a = 10
-	fmt.Println(&a, a, &b, b, c, *c)
-	*c = 20 //* = 주소에 있는 값
-	fmt.Println(*c, a)
-	*/
-	/*
+				account := accounts.NewAccount("sk")
+				account.Deposit(10)
+
+					fmt.Println(account.Balance())
+					err := account.Withdraw(20)
+					if err != nil {
+						fmt.Println(err)
+					}
+
+				fmt.Println(account)
+		struct
+		favFood := []string{"ricenoodle", "strawberry"}
+		sk := person{"sk", 26, favFood}
+		fmt.Println(sk.name, sk.age, sk.favFood)
+			sk := map[string]string{"name": "sk", "age": "12"} //key:value 형태
+			fmt.Println(sk)
+			for key, value := range sk {
+				fmt.Println(key, value)
+			}
+			names1 := [5]string{"sk", "kim", "kyu"} //Array
+			names2 := []string{"sk", "kim", "kyu"}  // Slice = 크기 제한 없는 Array
+			names1[3] = "heon"
+			names1[4] = "jin"
+			//names1[5] = "hee" //array값을 벗어남
+			//names2[3] = "heon" //Slice에 새로운 값을 추가할 경우에는 append함수 사용
+			names2 = append(names2, "jin")
+			fmt.Println(names1)
+			fmt.Println(names2)
+		a := 2
+		b := a
+		c := &a //& = 주소
+		a = 10
+		fmt.Println(&a, a, &b, b, c, *c)
+		*c = 20 //* = 주소에 있는 값
+		fmt.Println(*c, a)
 		fmt.Println(canIDrink(18))
 		superAdd(1, 2, 3, 4, 5, 6)
 		name := "sk" // var name string = "sk"와 같다, func안에서만 작동한다.
